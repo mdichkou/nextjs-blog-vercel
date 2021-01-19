f46a0c487a67a46a2b571577f8756ce96ee29dd1
Mohamed Amine Dichkour <mohamed.amine.dichkour@gmail.com>
2021-01-19 15:37:49 +0100

    NextJs Tuto

--- pages/index.js
+++ pages/index.js
@@ -23,8 +23,8 @@ export default function Home({ allPostsData }) {
     <section className={utilStyles.headingMd}>
       <p>GaddBox Team We Rock!</p>
       <p>
-        (This is a sample website - you’ll be building a site like this on{' '}
-        <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        (This is my first Project using
+        <a href="https://nextjs.org/learn">Next.js tutorial</a>.)
       </p>
     </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
